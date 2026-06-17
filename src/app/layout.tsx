ab19e3ad940a2adfa06a9d61063226118342a878
leiriele <leiricorrea@gmail.com>
2026-06-17 19:03:03 -0300

    alterar layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,19 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "TCC Leiriele",
   description: "Otimização de problemas de programação linear inteira",
+  openGraph: {
+    title: "TCC Leiriele",
+    description: "Otimização de problemas de programação linear inteira",
+    url: "https://projeto-tcc-gules.vercel.app/",
+    siteName: "TCC Leiriele",
+    locale: "pt_BR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary",
+    title: "TCC Leiriele",
+    description: "Otimização de problemas de programação linear inteira",
+  },
 };
 
 export default function RootLayout({
